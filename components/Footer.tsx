a40867d64c230155cea44fafc8436f6b76edd73f
yumekiti <yumekiti120440@gmail.com>
2022-03-31 00:06:30 +0900

    `feat` header footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Footer.module.css'
+import { Text } from "@nextui-org/react";
+
+export default () => {
+  return (
+    <div className={styles.footer}>
+      <Text>COPYRIGHT &#169; 2022 yumekiti</Text>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,12 @@
 import type { NextPage } from "next";
 import { Grid, Textarea, Image, Text, Spacer } from "@nextui-org/react";
-
-import styles from '../styles/Home.module.css'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className={styles.header}>
-        <Spacer x={3} />
-        <div>
-          <Image
-            src="./icon.png"
-            alt="Default Image"
-            width={50}
-            height={50}
-          />
-        </div>
-        <Spacer x={1} />
-        <h1>Diff Sync Code</h1>
-        <ul className={styles.sns}>
-            <li>
-              <a href="https://twitter.com/yumekiti1204">
-                <img src="./twitter.png" className={styles.icon} loading="lazy" width="42" height="42"></img>
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/yumekiti/Diff_Sync_Code">
-                <img src="./github.png" className={styles.icon} loading="lazy" width="42" height="42"></img>
-              </a>
-            </li>
-          </ul>
-      </div>
-
+      <Header />
       <Grid.Container gap={2}>
         <Grid xs={6} justify="center">
           <Textarea aria-label="テキストエリア" id="textarea" fullWidth rows={20} />
@@ -49,10 +24,7 @@ const Home: NextPage = () => {
           <Textarea aria-label="テキストエリア" id="textarea" fullWidth rows={20} />
         </Grid>
       </Grid.Container>
-
-      <div className={styles.footer}>
-        <Text>COPYRIGHT &#169; 2022 yumekiti</Text>
-      </div>
+      <Footer />
     </>
   );
 };
