d253fd5ee63fa6eadd182b0c6ccb5ff5f35fc474
yumekiti <yumekiti120440@gmail.com>
2022-03-30 18:33:19 +0900

    `feat` nextui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app";
 
+import { NextUIProvider } from '@nextui-org/react';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
+import { Button } from '@nextui-org/react';
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1>Hello World!</h1>
+      <Button>Click me</Button>
     </div>
   );
 };
