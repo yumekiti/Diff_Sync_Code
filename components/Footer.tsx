782e0e96f990f5effbab5762b759e156b81c3fbd
yumekiti <yumekiti@users.noreply.github.com>
2022-03-31 21:39:44 +0000

    Apply Prettier Change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,12 @@
-import styles from '../styles/Footer.module.css'
-import { Text } from "@nextui-org/react";
+import styles from '../styles/Footer.module.css';
+import { Text } from '@nextui-org/react';
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <Text>COPYRIGHT &#169; 2022 yumekiti</Text>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
