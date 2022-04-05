f53f5058f1d2f200ab0b44c2f764c42d094c43e9
yumekiti <yumekiti@users.noreply.github.com>
2022-04-05 10:54:54 +0000

    Apply Prettier Change

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,7 +75,7 @@ const Home: NextPage = () => {
           <Explain />
         </Grid>
         <Grid xs={6}>
-          <Share url={'https://google.com'} text={'hogee'}/>
+          <Share url={'https://google.com'} text={'hogee'} />
         </Grid>
       </Grid.Container>
       <Footer />
