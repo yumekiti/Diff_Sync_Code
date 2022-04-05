341dae238b573020b060e0285494b839e7047615
yumekiti <yumekiti120440@gmail.com>
2022-04-05 18:57:09 +0900

    `add` Explain

--- components/Explain.tsx
+++ components/Explain.tsx
@@ -1,11 +1,12 @@
-import { Grid, Card, Text } from '@nextui-org/react';
+import { Card, Text } from '@nextui-org/react';
 
 const Explain = () => {
   return (
     <Card>
       <Text h4>どうやって使うん。</Text>
       <Text>
-        左右にコードをコピー＆ペーストするか入力したら差分が出るから。これでエラー探して。😎
+        右上の言語選択から比較するコーディング言語を選んでから<br />
+        左右にコードをコピー＆ペーストするか入力したら差分が出るから。これでエラー探して😎
       </Text>
       <Text small>まぁ、構文エラーか知らんけど。</Text>
     </Card>
